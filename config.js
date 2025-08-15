0101895db1cd18b4e683dcee4bfa7069fa08f496
frankboi548-byte <frankboi548@gmail.com>
2025-08-15 15:48:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~CRsWFbIR#fHv4G_Kq1McifJm03wPvNn6VvY8Wf0l1JbHM9uiKUQQ",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~qM0TQDIT#tPC8IvZIXRxDMaz8zUXBptFPa6BLeIY2fNnWmSR-Iho",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -15,13 +15,13 @@ AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
 AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS SHABAN-MD*",
 // set the auto reply massage on status reply
-WELCOME: process.env.WELCOME || "false",
+WELCOME: process.env.WELCOME || "true",
 // true if want welcome and goodbye msg in groups    
 ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
 // make true to know who dismiss or promoted a member in group  
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://ik.imagekit.io/mrshaban/Picsart_25-02-01_22-47-44-239.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://i.ibb.co/gMG0HpZx/shaban-md.jpg",
 // add custom menu and mention reply image url   
 BOT_NAME: process.env.BOT_NAME || "𝙁𝙏 Boi,*Frank  𝙁𝙏 ༆  ༆ 𓃠V5",
 // add bot namw here for menu
@@ -31,7 +31,7 @@ CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "false",
+DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
 OWNER_NUMBER: process.env.OWNER_NUMBER || "27783583068",
 // add your bot owner number
@@ -39,9 +39,9 @@ OWNER_NAME: process.env.OWNER_NAME || "MR𝙁𝙏 Boi,*Frank  𝙁𝙏 ༆  ༆ 
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Mʀ 𝙁𝙏 Boi,*Frank  𝙁𝙏 ༆  ༆ 𓃠*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/mrshaban/Picsart_25-02-01_22-47-44-239.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/gMG0HpZx/shaban-md.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *SHABAN-MD*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar * 𝙁𝙏 Boi,*Frank  𝙁𝙏 ༆  ༆ 𓃠*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -57,7 +57,7 @@ AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
 // make true or false automatic text reply 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
